8df10fece36033ae0899969b243dc8b952246735
EvgRass <johnrass@yandex.ru>
2022-10-04 08:12:05 +0300

    modified:   artisan

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,6 +1,6 @@
 <nav class="bg-white border-gray-200 py-2.5 dark:bg-gray-900 shadow-md">
     <div class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
-        <a href="/" class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+        <a href="/" class="flex items-center">
             <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">{{ __('messages.Task manager') }}</span>
         </a>
 
